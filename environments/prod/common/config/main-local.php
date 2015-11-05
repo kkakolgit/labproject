ebf1ac27df210182638a411481cbc9df9ee0f092
Krzysztof <kkakol@pgs-soft.com>
2015-11-05 19:55:59 +0100

    Initial commit

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=projectdb',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
